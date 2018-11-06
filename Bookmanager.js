423816290e47e91dcd694e893f95e8bf8edc747c
pndangduy <SEAL6SNIPER1294@GMAIL.COM>
2018-11-06 13:21:07 +0700

    Add files via upload

---
+++ Bookmanager.js
@@ -0,0 +1,18 @@
+﻿[‎11 /‎6 /‎2018 1: 12 PM] Nguyen Tan Vang(FA.G0):
+function deleteBook(bookid) {
+	if (bookid == 0) {
+		window.alert("Book is not exist!");
+	}
+	else {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200 && this.responseText == "True") {
+				var tr = "#tr" + bookid;
+				$(tr).css("display", "none");
+				window.alert("Delete book success!");
+			}
+		};
+		xhttp.open("DELETE", "Delete?Id=" + bookid, true);
+		xhttp.send();
+	}
+} 
